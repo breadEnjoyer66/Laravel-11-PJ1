2a03aa9796d79ce795f9b8bd6277932aa4c18d56
Qodri Khalik <qodrikhalik10@gmail.com>
2025-10-28 14:58:43 +0700

    modified warranty page

--- resources/views/warranty.blade.php
+++ resources/views/warranty.blade.php
@@ -4,7 +4,7 @@
 
     <section class="max-w-7xl mx-auto px-6 py-16 space-y-20">
         <!-- INTRO -->
-        <div class="grid md:grid-cols-2 gap-10 items-center">
+        <div class="grid md:grid-cols-2 lg:gap-14 gap-10 items-center">
             <div>
                 <h1 class="{{ $title_1 }}">Warranty Information</h1>
                 <p class="text-gray-600 leading-relaxed text-justify">
@@ -20,9 +20,8 @@
                 </p>
             </div>
             <div class="relative">
-                <img src="{{ asset('img/warranty-1.jpg') }}" alt="Warranty Illustration"
-                    class="rounded-2xl shadow-lg w-full h-80 object-cover">
-                <div class="absolute inset-0 rounded-2xl bg-gradient-to-t from-black/20 to-transparent"></div>
+                <img src="{{ asset('img/warranty-2.jpg') }}" alt="Warranty Illustration"
+                    class="rounded-2xl w-full h-96 object-contain">
             </div>
         </div>
 
